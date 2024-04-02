f11917c9b613af769832c13d716795aa8af20727
Yin, Wesley <71534272+Orpheus-yjk@users.noreply.github.com>
2024-04-02 15:25:39 +0800

    Delete symbolic analysis tool/Honeybadger-fixed/honeybadger directory

--- symbolic analysis tool/Honeybadger-fixed/honeybadger/ast_walker.py
+++
@@ -1,8 +0,0 @@
-class AstWalker:
-    def walk(self, node, node_name, nodes):
-        if node["name"] == node_name:
-            nodes.append(node)
-        else:
-            if "children" in node and node["children"]:
-                for child in node["children"]:
-                    self.walk(child, node_name, nodes)
